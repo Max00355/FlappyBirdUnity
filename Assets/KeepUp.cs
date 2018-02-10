317257710e9f78d34e76339ae1425617d9638d5e
Frankie <frankieprimerano@gmail.com>
2018-02-10 16:19:08 -0500

    Jumping and pipes are getting there

---
+++ Assets/KeepUp.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepUp : MonoBehaviour
+{
+	public Vector2 position = new Vector2 (0, 0);
+	public float gravity = 0.001f;
+	public float gravity_max = 0.3f;
+	public float gravity_at = 0;
+	public float jumping = 0;
+
+	void Start ()
+	{
+		
+	}
+
+	void Jump() {
+		position.y += jumping;
+		jumping -= 0.01f;
+	}
+
+	void Gravity() {
+		if (this.gravity_at < this.gravity_max) {
+			this.gravity_at += this.gravity;
+		}
+
+		this.position.y -= this.gravity_at;
+	}
+
+	void Update ()
+	{	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			jumping = 0.1f;
+			gravity_at = 0;
+		}
+
+		if (jumping == 0) {
+			this.Gravity ();
+		} else {
+			this.Jump ();
+		}
+		gameObject.transform.position = this.position;
+
+	}
+}
+
---
+++ Assets/MovePipes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipes : MonoBehaviour {
+
+	private float height;
+	private float width;
+	public float speed = 0.03F;
+	private float offset_direction = -1;
+
+	void Start () {
+		this.height = 2f * Camera.main.orthographicSize;
+		this.width = this.height / 2 * Camera.main.aspect;
+
+	}
+				
+	float GetOffset() {
+		offset_direction *= -1;
+		return offset_direction * -1 * Random.value;
+	}
+
+	void Update () {
+		GameObject top_pipe = GameObject.Find("top_pipe");
+		GameObject bottom_pipe = GameObject.Find("bottom_pipe");
+		Vector2 tpv = top_pipe.transform.position;
+		Vector2 bpv = bottom_pipe.transform.position;
+
+		tpv.x -= this.speed;
+		bpv.x -= this.speed;
+
+		if (bpv.x < -this.width) {
+			float offset = this.GetOffset ();
+			tpv.y += offset;
+			bpv.y += offset;
+			bpv.x = this.width;
+			tpv.x = this.width;
+		}
+
+		top_pipe.transform.position = tpv;
+		bottom_pipe.transform.position = bpv;
+	}
+		
+}
